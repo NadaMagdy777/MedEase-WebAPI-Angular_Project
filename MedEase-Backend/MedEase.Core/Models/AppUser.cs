ac087e62db8520fef3cb844876372a87117a857f
Abdallah Assaker <abdallahassaker@gmail.com>
2023-04-13 14:34:54 +0200

    comments

--- MedEase-Backend/MedEase.Core/Models/AppUser.cs
+++ MedEase-Backend/MedEase.Core/Models/AppUser.cs
@@ -10,12 +10,12 @@ namespace MedEase.Core.Models
 {
     public class AppUser : IdentityUser
     {
-        public string FirstName { get; set; }
+        public string FirstName { get; set; }     //=> name
         public string LastName { get; set; }
         public string Phone { get; set; }
-        public string SSN { get; set; }
-        public DateTime BirthDate { get; set; }
-        public Gender Gender { get; set; }
+        public string SSN { get; set; }        //==> ignore
+        public DateTime BirthDate { get; set; }     //==>Age
+        public Gender Gender { get; set; }          //==>get   
         public virtual Address Address { get; set; }
         public virtual Doctor? Doctor {get; set; }
         public virtual Patient? Patient { get; set; }
--- MedEase-Backend/MedEase.EF/Services/DoctorService.cs
+++ MedEase-Backend/MedEase.EF/Services/DoctorService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MedEase.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace MedEase.EF.Services
 {
-    internal class DoctorService
+    public class DoctorService : IDoctorService
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DoctorService(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+
+
     }
 }
