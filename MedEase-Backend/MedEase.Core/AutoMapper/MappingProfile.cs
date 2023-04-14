19340d22302ff1cf744f13365dedc39699822ced
Elshaymaa Ahmed <elshaymaaahmed@gmail.com>
2023-04-14 07:38:50 +0200

    Add autoMapper

---
+++ MedEase-Backend/MedEase.Core/AutoMapper/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using MedEase.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using MedEase.Core.Dtos;
+
+namespace MedEase.Core.AutoMapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Appointment, ReserveAppointmentDto>().ReverseMap();
+        }
+    }
+}
