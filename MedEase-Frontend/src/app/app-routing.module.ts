f0b4ced00dd9a58b1bcb968016e88690d4bf9b8b
moeen1998 <58312233+moeen1998@users.noreply.github.com>
2023-04-21 23:30:41 +0200

    Nav and Footer And Authentication

--- MedEase-Frontend/src/app/app-routing.module.ts
+++ MedEase-Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { AuthGuard } from './Authentication/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home",component:FooterComponent,canActivate:[AuthGuard]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
