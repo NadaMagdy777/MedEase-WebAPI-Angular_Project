6021583b5802ea195e15ee0ce60f7ae8db1dbcf9
Elshaymaa Ahmed <elshaymaaahmed@gmail.com>
2023-04-27 14:13:31 +0200

    home

--- MedEase-Frontend/src/app/app-routing.module.ts
+++ MedEase-Frontend/src/app/app-routing.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FooterComponent } from './components/layout/footer/footer.component';
 import { AuthGuard } from './Authentication/auth.guard';
 import { ProfileComponent } from './components/Patient/profile/profile.component';
 import { AppointmentsComponent } from './components/Patient/appointments/appointments.component';
 import { InsuranceComponent } from './components/Patient/insurance/insurance.component';
+import { HomeComponent } from './components/home/home/home.component';
 
 const routes: Routes = [
-  {path:"home",component:FooterComponent,canActivate:[AuthGuard]},
+  {path:"home",component:HomeComponent,canActivate:[AuthGuard]},
   {path:"doctor",loadChildren:()=>import('./modules/Doctor/doctor/doctor.module').then(mod=>mod.DoctorModule)},
   {path:'accountProfile',component:ProfileComponent},
   {path:'userAppointments',component:AppointmentsComponent},
-  {path:'userInsurance',component:InsuranceComponent}
+  {path:'userInsurance',component:InsuranceComponent},
+
+
+
+  {path:"**",component:HomeComponent},
 ];
 
 @NgModule({
